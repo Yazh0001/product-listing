9c429166e166161817e9d11ff10b4131abeb14c0
unknown <Yazh0001@student.monash.edu>
2022-09-14 21:20:24 +0800

    Added blobs and title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
-
+import React from 'react';
+import './product.css'
 function App() {
   return (
-    <div className="App">
+    <div>
+      <img id='VectorTopLeft' src={require('./Assets/Images/VectorTopLeft.png')} alt='plant'></img>
+      <img id='VectorMidRight' src={require('./Assets/Images/VectorMidRight.png')} alt='plant'></img>
+      <img id='VectorTopMid' src={require('./Assets/Images/VectorTopMid.png')} alt='plant'></img>
+      <img id='VectorMidMid' src={require('./Assets/Images/VectorMidMid.png')} alt='plant'></img>
+      <img id='VectorMidLeft' src={require('./Assets/Images/VectorMidLeft.png')} alt='plant'></img>
+      
+      <div className='title'>LOREM IPSUM DOLOR SIT AMET.</div>
     </div>
   );
 }
