5ab1bac20835c6086d2b071ec4ff797aa3cae0e3
unknown <Yazh0001@student.monash.edu>
2022-09-14 22:17:21 +0800

    Added buttons and nav

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React from 'react';
 import './product.css'
 function App() {
   return (
-    <div>
-      <img id='VectorTopLeft' src={require('./Assets/Images/VectorTopLeft.png')} alt='plant'></img>
-      <img id='VectorMidRight' src={require('./Assets/Images/VectorMidRight.png')} alt='plant'></img>
-      <img id='VectorTopMid' src={require('./Assets/Images/VectorTopMid.png')} alt='plant'></img>
-      <img id='VectorMidMid' src={require('./Assets/Images/VectorMidMid.png')} alt='plant'></img>
-      <img id='VectorMidLeft' src={require('./Assets/Images/VectorMidLeft.png')} alt='plant'></img>
+    <>
+      <div className='blobs'>
+        <img id='VectorTopLeft' src={require('./Assets/Images/VectorTopLeft.png')} alt='plant'></img>
+        <img id='VectorMidRight' src={require('./Assets/Images/VectorMidRight.png')} alt='plant'></img>
+        <img id='VectorTopMid' src={require('./Assets/Images/VectorTopMid.png')} alt='plant'></img>
+        <img id='VectorMidMid' src={require('./Assets/Images/VectorMidMid.png')} alt='plant'></img>
+        <img id='VectorMidLeft' src={require('./Assets/Images/VectorMidLeft.png')} alt='plant'></img>
       
-      <div className='title'>LOREM IPSUM DOLOR SIT AMET.</div>
-    </div>
+      
+      </div>
+      <div className='container1'>
+        <div className='title'>LOREM IPSUM DOLOR SIT AMET.</div>
+        <button>Read More</button>
+      </div>
+      <nav>
+        <div id='topgrape' className='grape'>Chilled Grape</div>
+        <div id='options'>
+          <div className='option'>Home</div>
+          <div className='option'>About</div>
+          <div className='option'>Blog</div>
+          <div className='option'>Pricing</div>
+        </div>
+        <button id='login' >Login</button>
+      </nav>
+    </>
   );
 }
 
