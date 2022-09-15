96cbbc535bc7975eedb1de7e47427c1f1e4b4099
unknown <Yazh0001@student.monash.edu>
2022-09-15 11:48:17 +0800

    Added footer

--- src/ProductList.js
+++ src/ProductList.js
@@ -10,6 +10,37 @@ export default function ProductList({objects}) {
         <div className='bottom'>
           <img id='VectorBot' src={require('./Assets/Images/VectorBot.png')} alt='blob'></img>
           <div id='botGrape' className='grape'>CHILLED GRAPE</div>
+          <div id='botParagraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.</div>
+          
+          <div className='container3'>
+            <div className='footerBlock'>
+              <div className='footerTitle'>Company</div>
+              <div className='footerItem'>About</div>
+              <div className='footerItem'>Careers</div>
+              <div className='footerItem'>Our Work</div>
+              <div className='footerItem'>Contact</div>
+            </div>
+            <div className='footerBlock'>
+              <div className='footerTitle'>Products</div>
+              <div className='footerItem'>About</div>
+              <div className='footerItem'>Careers</div>
+              <div className='footerItem'>Our Work</div>
+              <div className='footerItem'>Contact</div>
+            </div>
+            <div className='footerBlock'>
+              <div className='footerTitle'>Follow Us</div>
+              <div>
+                <div className='followBox'></div>
+                <div className='followBox'></div>
+                <div className='followBox'></div>
+                <div className='followBox'></div>
+                <div className='followBox'></div>
+                <div className='followBox'></div>
+              </div>
+            </div>
+          </div>
+
         </div>
     </div>
   )
